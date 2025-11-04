62e0a2a2b538a5e4093fe2042cb469ab2556fded
leodera <pascalchidera21@gmail.com>
2025-11-04 21:26:59 +0100

    Add files via upload

--- upload.js
+++ upload.js
@@ -1,17 +1,17 @@
-import * as Client from '@storacha/client'
-import { filesFromPaths } from 'files-from-path'
+import { client } from "@storacha/client"
+import { filesFromPaths } from "files-from-path"
 
-// --- Your Account Email ---
+// --- REPLACE THIS EMAIL WITH YOUR ACCOUNT EMAIL ---
 const YOUR_EMAIL = 'pascalchidera21@gmail.com'
 const FILE_TO_UPLOAD = 'test-file.txt'
 
 async function run() {
   try {
     console.log('1. Initializing Storacha Client...')
-    const client = await Client.create()
+    const client = await client.Client.create()
 
     console.log('2. Logging in and using existing credentials...')
-    await client.login(YOUR_EMAIL) 
+    await client.login(YOUR_EMAIL)
 
     console.log(`3. Preparing file for upload: ${FILE_TO_UPLOAD}`)
     const files = await filesFromPaths([FILE_TO_UPLOAD])
@@ -20,7 +20,6 @@ async function run() {
     const cid = await client.uploadDirectory(files)
 
     const gatewayUrl = `https://storacha.link/ipfs/${cid}`
-
     console.log('\n✅ UPLOAD SUCCESSFUL!')
     console.log(`IPFS CID: ${cid}`)
     console.log(`Gateway URL: ${gatewayUrl}`)
